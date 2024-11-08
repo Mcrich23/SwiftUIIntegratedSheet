a75172124b725b9a8721c7ad9f623a4f2866abd4
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-11-08 11:10:53 -0800

    Code Cleanup

--- Sources/SwiftUIIntegratedSheet/SheetContainer.swift
+++ Sources/SwiftUIIntegratedSheet/SheetContainer.swift
@@ -10,15 +10,13 @@ import SwiftUI
 public struct SheetContainer<Header: View, Content: View>: View {
     let title: String
     @Binding var isShown: Bool
-    @Binding var sheetScrollOffset: CGFloat
     private var header: (() -> Header)?
     private var content: () -> Content
     @State var scrollOffsetIsDisabled = false
     
-    public init(title: String, isShown: Binding<Bool>, sheetScrollOffset: Binding<CGFloat>? = nil, @ViewBuilder header: @escaping () -> Header, @ViewBuilder content: @escaping () -> Content) {
+    public init(title: String, isShown: Binding<Bool>, @ViewBuilder header: @escaping () -> Header, @ViewBuilder content: @escaping () -> Content) {
         self.title = title
         self._isShown = isShown
-        self._sheetScrollOffset = sheetScrollOffset ?? Binding(get: {0}, set: {_ in})
         self.header = header
         self.content = content
     }
@@ -26,7 +24,6 @@ public struct SheetContainer<Header: View, Content: View>: View {
     public init(title: String, isShown: Binding<Bool>, sheetScrollOffset: Binding<CGFloat>? = nil, @ViewBuilder content: @escaping () -> Content) where Header == EmptyView {
         self.title = title
         self._isShown = isShown
-        self._sheetScrollOffset = sheetScrollOffset ?? Binding(get: {0}, set: {_ in})
         self.header = nil
         self.content = content
     }
@@ -70,41 +67,14 @@ public struct SheetContainer<Header: View, Content: View>: View {
                     }
                     Color.clear.frame(height: 15)
                 }
-//                .onPreferenceChange(ViewOffsetKey.self) { (offset: CGFloat) in
-//                    guard (offset < 0 || sheetScrollOffset >= 0), !scrollOffsetIsDisabled else { return }
-//                    
-//                    guard sheetScrollOffset < 160 else {
-//                        hideFromScroll()
-//                        return
-//                    }
-//                    
-//                    sheetScrollOffset -= offset
-//                }
                 .onChange(of: isShown) {
                     scrollOffsetIsDisabled = !isShown
                 }
             }
-//            .onScrollPhaseChange({ oldPhase, newPhase in
-//                if [ScrollPhase.idle, .tracking, .decelerating].contains(newPhase) && sheetScrollOffset > 120 { // Hide if at threshold and sheet stopping
-//                    hideFromScroll()
-//                } else if [ScrollPhase.idle, .tracking].contains(newPhase) { // Else don't hide if stopping and not at threshold
-//                    withAnimation {
-//                        sheetScrollOffset = 0
-//                    }
-//                }
-//            })
             .coordinateSpace(name: "scroll")
             .ignoresSafeArea()
         }
         .padding(.horizontal)
-    }
-    
-    func hideFromScroll() {
-        scrollOffsetIsDisabled = true
-        withAnimation {
-            isShown = false
-            sheetScrollOffset = 0
-        }
     }
 }
 
